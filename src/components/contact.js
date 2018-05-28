cd6b68e19b75614b80726fceff318933aa277520
Maribel Duran <maribelduranucm@gmail.com>
2018-05-28 08:15:25 -0700

    Transform form inputs into react-materialize components

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,37 +1,20 @@
 import React from "react";
 import  "./contact.css";
+import { Button, Row, Icon, Input } from 'react-materialize';
 
 const Contact = () =>
 <section className="contact" >
-  <h3>Have more questions?</h3>
+  <h3>Have more questions? </h3>
   <h3>Send me a message!</h3>
-    <div className="row">
-    <form className="grey-border-shadow">
-      <div className="row"> 
-        <div className="input-field col s12">
-            <input id="first_name" type="text" className="validate" />
-            <label for="first_name">Name</label>
-            </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s12">
-          <input id="email" type="email" className="validate" />
-          <label for="email">Email</label>  
-          </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s12">
-            <textarea id="textarea1" class="materialize-textarea"></textarea>
-            <label for="textarea1">Message</label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s12">
-            <input className="waves-effect waves-light btn-large" type="submit" value="Send" />
-        </div>
-      </div>
-    </form>
-   </div>
+  
+  <form className="grey-border-shadow">
+    <Row>
+      <Input s={12} type="text"  id="first_name" label="Name" validate="true" icon="account_circle" />
+      <Input s={12} type="email" id="email" label="Email" validate="true" icon="email"/>
+      <Input s={12} type='textarea' id="message" label="Message" icon="mode_edit" />
+      <Button type='submit' waves='light'>Submit<Icon right>send</Icon></Button>
+    </Row>
+  </form>
 </section>
 
 export default Contact;
