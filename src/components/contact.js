8f10302c9a1b2f22e499ab09a2e2ed5e77a80109
Maribel Duran <maribelduranucm@gmail.com>
2018-05-29 13:16:40 -0700

    Add a controlled contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,20 +1,70 @@
-import React from "react";
-import  "./contact.css";
+import React, {Component} from 'react';
 import { Button, Row, Icon, Input } from 'react-materialize';
+import  "./contact.css";
 
-const Contact = () =>
-<section className="contact" >
-  <h3>Have more questions? </h3>
-  <h3>Send me a message!</h3>
+const PATH_BASE = 'https://formspree.io';
+const PATH_EMAIL_TO = '/maribelduranucm@gmail.com';
+
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: '',
+      email:'',
+      message: '',
+      messageError: ''
+
+    };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+  onInputChange(event){
+    if (event.target.name === "message"){
+      this.setState({ 
+        [event.target.name]: event.target.value,
+        messageError: "",
+      });
+    }else{
+      this.setState({ [event.target.name]: event.target.value});
+    }
+   
+  }
+  onFormSubmit(event){
+    if(this.state.message.length === 0){
+      event.preventDefault();
+      this.setState({ messageError: "message is required"});
+    }
+    else{
+      this.setState({ messageError: ""});
+    }
+  }
+  render() {
+    const url = `${PATH_BASE}${PATH_EMAIL_TO}`;
+    
+    return(
+      <section className="contact" >
+      <h3>Have more questions? </h3>
+      <h3>Send me a message!</h3>
   
-  <form className="grey-border-shadow">
-    <Row>
-      <Input s={12} type="text"  id="first_name" label="Name" validate="true" icon="account_circle" />
-      <Input s={12} type="email" id="email" label="Email" validate="true" icon="email"/>
-      <Input s={12} type='textarea' id="message" label="Message" icon="mode_edit" />
-      <Button type='submit' waves='light'>Submit<Icon right>send</Icon></Button>
-    </Row>
-  </form>
-</section>
+      <form className="grey-border-shadow" action={url}
+      method="POST" onSubmit={this.onFormSubmit}>
+        <Row>
+          <Input s={12} type="text" name="fullName" label="Name" validate={true} icon="account_circle" onChange={this.onInputChange} value={this.state.fullName} />
+          <Input s={12} type="email" name="email" label="Email" validate={true} icon="email" onChange={this.onInputChange} value={this.state.email}/>
+          <Input s={12}
+            type='textarea' name="message" 
+            label="Message" icon="mode_edit"
+            error={this.state.messageError} 
+            validate={true}
+            onChange={this.onInputChange} 
+            value={this.state.message} />
+          <input type="text" name="_gotcha" style={{display:"none"}} />
+          <Button type='submit' waves='light'>Submit<Icon right>send</Icon></Button>
+        </Row>
+      </form>
+    </section>
+    );
+  }
+}
 
 export default Contact;
