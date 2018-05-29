72e655871e474d23f59622a04e77672415fc4819
Maribel Duran <maribelduranucm@gmail.com>
2018-05-29 14:46:28 -0700

    Add accessibility attributes

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import  "./footer.css";
 
 const Footer = () =>
 <footer>
-  <p>Made With Love <span role="img">&#128153;</span> By <a href="http://www.maribelduran.com/" target="_blank" rel="noopener noreferrer">Maribel Duran</a></p>
+  <p>Made With Love <span role="img" aria-label="Blue Heart">&#128153;</span>By <a href="http://www.maribelduran.com/" target="_blank" rel="noopener noreferrer">Maribel Duran</a></p>
 </footer>
 
 export default Footer;
