96b89776541c9c47121dd2fe21a68b7ad14dc8c7
Maribel Duran <maribelduranucm@gmail.com>
2018-05-29 15:39:22 -0700

    Update card-action element

--- src/components/question_answer.js
+++ src/components/question_answer.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Row, Col} from 'react-materialize';
 import  "./question_answer.css";
+
 import questionsAndAnswers from "../data/QandA.json"
 
 const QandA = () =>
 <section className="q-and-a">
+<h3>Q & A</h3>
   <div className='question-list animated lightSpeedIn'>
     {questionsAndAnswers.map( item => 
       <div className="single-card grey-border-shadow">
@@ -11,9 +14,13 @@ const QandA = () =>
         <p className="answer">{item.answer}</p>
         { item.links.length > 0 ? 
             <div className="card-action">
+            <Row>
               {item.links.map(link => 
+              <Col s={6} className='card-link-item'>
               <a href={link} target="_blank" >Check it out</a>
+              </Col>
               )}
+              </Row>
             </div> 
           : null
         }
