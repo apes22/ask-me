47c78d2aec1337027e2ea5d1fdbfee706b98a53a
Maribel Duran <maribelduranucm@gmail.com>
2018-05-29 15:32:57 -0700

    Update width percentage for form and padlet classes

--- src/components/contact.js
+++ src/components/contact.js
@@ -13,7 +13,6 @@ class Contact extends Component {
       email:'',
       message: '',
       messageError: ''
-
     };
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
@@ -27,7 +26,6 @@ class Contact extends Component {
     }else{
       this.setState({ [event.target.name]: event.target.value});
     }
-   
   }
   onFormSubmit(event){
     if(this.state.message.length === 0){
@@ -62,6 +60,20 @@ class Contact extends Component {
           <Button type='submit' waves='light'>Submit<Icon right>send</Icon></Button>
         </Row>
       </form>
+      <div className="padlet">
+      <h3>You can also ask me on Padlet</h3>
+      <div className="padlet-embed" style={{border:"1px solid rgba(0,0,0,0.1)", borderRadius:"2px; box-sizing:border-box", overflow:"hidden", position:"relative", width:"100%", background:"#F4F4F4"}}>
+        <p style={{padding:"0", margin:"0"}}>
+          <iframe title="Padlet" src="https://padlet.com/embed/35us5t10nzes?rel=0" frameBorder="0" allow="camera;microphone;geolocation" style={{width:"100%", height:"608px", display:"block", padding:"0", margin:"0"}}>
+          </iframe>
+        </p>
+        <div style={{padding:"8px", textAlign:"right", margin:"0"}}>
+          <a href="https://padlet.com?ref=embed" style={{padding:"0", margin:"0", border:"none", display:"block", lineHeight:"1", height:"16px"}} target="_blank" rel="noopener noreferrer">
+            <img src="https://resources.padletcdn.com/assets/made_with_padlet.png" width="86" height="16" style={{padding:"0", margin:"0", background:"none", border:"none", display:"inline",  boxShadow:"none"}} alt="Made with Padlet"></img>
+          </a>
+        </div>
+      </div>
+      </div>
     </section>
     );
   }
