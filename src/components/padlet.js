61e5f9daa89ee6d900a1c28aa8ec5189e45461bd
Maribel Duran <maribelduranucm@gmail.com>
2018-07-13 21:35:58 -0700

    Add padlet component

---
+++ src/components/padlet.js
@@ -0,0 +1,22 @@
+import React from "react";
+import  "./padlet.css";
+
+
+const Padlet = () =>
+<div className="padlet">
+  <h3>You can also ask me on Padlet</h3>
+  
+  <div className="padlet-embed">
+    <p style={{padding:"0", margin:"0"}}>
+      <iframe className="padlet-iframe" title="Padlet" src="https://padlet.com/embed/35us5t10nzes?rel=0" frameBorder="0" allow="camera;microphone;geolocation">
+      </iframe>
+    </p>
+    <div style={{padding:"8px", textAlign:"right", margin:"0"}}>
+      <a href="https://padlet.com?ref=embed" style={{padding:"0", margin:"0", border:"none", display:"block", lineHeight:"1", height:"16px"}} target="_blank" rel="noopener noreferrer">
+        <img src="https://resources.padletcdn.com/assets/made_with_padlet.png" width="86" height="16" alt="Made with Padlet"></img>
+      </a>
+    </div>
+  </div>
+</div>
+
+export default Padlet;
