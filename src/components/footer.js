9a6f32c973ae0c10dd689729ed1fd853cf4243b1
Maribel Duran <maribelduranucm@gmail.com>
2018-07-13 21:07:04 -0700

    Add github repo link

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,7 @@ import  "./footer.css";
 
 const Footer = () =>
 <footer>
+<p>Github Repo <a href="https://github.com/maribelduran/ask-me" target="_blank"><span class="fa fa-github"></span></a></p>
   <p>Made With Love <span role="img" aria-label="Blue Heart">&#128153;</span>By <a href="http://www.maribelduran.com/" target="_blank" rel="noopener noreferrer">Maribel Duran</a></p>
 </footer>
 
